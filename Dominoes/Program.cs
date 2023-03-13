6bc3e4bff8469e9dcc106cfe2602fe93d7112b35
BrisaAguayo <124205663+BrisaAguayo@users.noreply.github.com>
2023-03-13 19:46:57 +0000

    commit

---
+++ Dominoes/Program.cs
@@ -0,0 +1,31 @@
+﻿class Dominoes
+{
+    //Declaramos nuestros parámetros
+    int arriba;
+    int abajo;
+
+    //asignamos a nuestra clase los parametros, dónse se pondrán
+    public Dominoes(int arriba, int abajo)
+    {
+        this.arriba = arriba;
+        this.abajo = abajo;
+    }
+
+    public static int operator + (Dominoes a, Dominoes b)
+    {
+        int suma = a.arriba + b.arriba + a.abajo + b.abajo;
+        return suma;
+    }
+}
+internal class Program 
+{
+    private static void Main(string[] args)
+    {
+       Dominoes d1 = new Dominoes(0,1);
+       Dominoes d2 = new Dominoes(2,3);
+       int suma  = d1 + d2;
+       Console.WriteLine(suma);
+
+    }
+
+}
